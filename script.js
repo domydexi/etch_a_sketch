6d2e6954dc03045e6e90a5de90fa33201045fb7d
domydexi <dominikdomy007@gmail.com>
2022-01-04 16:53:38 +0100

    Create script and style file

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("container");
+
+//Creates the grid
+function makeGrid (rows, cols) {
+
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for(c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeGrid(16,16);
