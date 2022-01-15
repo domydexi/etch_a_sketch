587f1cbde5ea39ef2557b90aa4d2b0bea6b3aaf7
domydexi <dominikdomy007@gmail.com>
2022-01-15 13:44:47 +0100

    Rewriting code for drawing

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+const D_SIZE = 16;
+const D_MODE = 'color';
+const D_COLOR = '#FFFFFF';
+
+let currentColor = D_COLOR;
+let currentMode = D_MODE;
+let currentSize = D_SIZE;
+
+
+function setColor(newColor){
+    currentColor = newColor;
+}
+
+function setMode(newMode){
+    unlockBtn(newMode);
+    currentMode = newMode;
+}
+
+function setSize(newSize){
+    currentSize = newSize;
+}
+
+
+let cPicker = document.getElementById("cPicker");
+let colBtn = document.getElementById("colBtn");
+const clearBtn = document.getElementById("clBtn");
+let sizeValue = document.getElementById("sizeValue");
+let slider = document.getElementById("slider");
 const container = document.getElementById("container");
 
+cPicker.onchange = (e) => setColor(e.target.value);
+
 //Creates the grid
-function makeGrid (rows, cols) {
+function makeGrid (size) {
 
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for(c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
+    for(let i = 0; i < (size * size); i++) {
+        const gridPiece = document.createElement("div");
+        gridPiece.addEventListener("mouseover", colorChange);
+        container.appendChild(gridPiece);
     };
 };
 
-container.addEventListener("mouseover", function(event){
-    event.target.classList.replace("grid-item", "color");
-});
+function colorChange(event){
 
-makeGrid(20,20);
+}
+
+makeGrid(D_SIZE);
