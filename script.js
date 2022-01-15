51ed4cc5e3464637ee1d9bb8b77dfa77862e2c90
domydexi <dominikdomy007@gmail.com>
2022-01-15 15:45:52 +0100

    Add functions for drawing, creating grid and buttons

--- script.js
+++ script.js
@@ -1,50 +1,95 @@
-const D_SIZE = 16;
+const D_COLOR = '#b7b5e4';
 const D_MODE = 'color';
-const D_COLOR = '#FFFFFF';
+const D_SIZE = 16;
 
 let currentColor = D_COLOR;
 let currentMode = D_MODE;
 let currentSize = D_SIZE;
 
-
-function setColor(newColor){
-    currentColor = newColor;
+function setCurrentColor(newColor) {
+  currentColor = newColor;
 }
 
-function setMode(newMode){
-    unlockBtn(newMode);
-    currentMode = newMode;
+function setCurrentMode(newMode) {
+  activateButton(newMode);
+  currentMode = newMode;
 }
 
-function setSize(newSize){
-    currentSize = newSize;
+function setCurrentSize(newSize) {
+  currentSize = newSize;
 }
 
+const cPicker = document.getElementById('cPicker');
+const colBtn = document.getElementById('colBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const clearBtn = document.getElementById('clBtn');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const grid = document.getElementById('grid');
 
-let cPicker = document.getElementById("cPicker");
-let colBtn = document.getElementById("colBtn");
-const clearBtn = document.getElementById("clBtn");
-let sizeValue = document.getElementById("sizeValue");
-let slider = document.getElementById("slider");
-const container = document.getElementById("container");
+cPicker.onchange = (e) => setCurrentColor(e.target.value);
+colBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
 
-cPicker.onchange = (e) => setColor(e.target.value);
-
-//Creates the grid
-function makeGrid (size) {
-
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    for(let i = 0; i < (size * size); i++) {
-        const gridPiece = document.createElement("div");
-        gridPiece.addEventListener("mouseover", colorChange);
-        container.appendChild(gridPiece);
-    };
-};
-
-function colorChange(event){
-
+function changeSize(value) {
+  setCurrentSize(value);
+  updateSizeValue(value);
+  reloadGrid();
 }
 
-makeGrid(D_SIZE);
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`;
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(currentSize);
+}
+
+function clearGrid() {
+  grid.innerHTML = '';
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.addEventListener('mouseover', changeColor);
+    grid.appendChild(gridElement);
+  }
+}
+
+function changeColor(e) {
+  if (currentMode === 'rainbow') {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor;
+  }
+}
+
+function activateButton(newMode) {
+  if (currentMode === 'rainbow') {
+    rainbowBtn.classList.remove('active');
+  } else if (currentMode === 'color') {
+    colBtn.classList.remove('active');
+  }
+
+  if (newMode === 'rainbow') {
+    rainbowBtn.classList.add('active');
+  } else if (newMode === 'color') {
+    colBtn.classList.add('active');
+  }
+}
+
+window.onload = () => {
+  setupGrid(D_SIZE);
+  activateButton(D_MODE);
+}
